6bcf60bd236ce36f3c2ee2e2ac9e1457ec0a46d2
Winky-2 <winkyzhuang@gmail.com>
2026-09-11 10:31:52 +0800

    改個名

--- A1_preprocess_before_annotation.py
+++ A1_preprocess_before_annotation.py
@@ -129,7 +129,7 @@ def enhance_image(img, clip_limit=CLAHE_CLIP_LIMIT, tile_grid_size=CLAHE_TILE_GR
 # ============================================================
 
 INPUT_DIR = "Data_second"        # 👈 原始、尚未標註的X光片資料夾
-OUTPUT_DIR = "second_enhanced"   # 👈 增強後、要拿去Roboflow標註的輸出資料夾
+OUTPUT_DIR = "second_A1"   # 👈 增強後、要拿去Roboflow標註的輸出資料夾
 
 CLIP_LIMIT = CLAHE_CLIP_LIMIT
 TILE_GRID_SIZE = CLAHE_TILE_GRID_SIZE
